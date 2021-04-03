991e8df9d39285df2353480a0ada8a5d3858d7a0
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-04-03 14:49:59 +0200

    Advanced Authentication & Beginning of Validation

--- node_course/express/controllers/auth.js
+++ node_course/express/controllers/auth.js
@@ -1,8 +1,10 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
 const flash = require("connect-flash");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
+const { validationResult } = require("express-validator/check");
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
@@ -59,6 +61,16 @@ exports.postSignup = async (req, res, next) => {
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
   const hashedPassword = await bcrypt.hash(password, 12);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      isAuthenticated: false,
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+
   User.findOne({ email })
     .then((userDocument) => {
       if (userDocument) {
@@ -87,5 +99,41 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
     res.redirect("/");
+  });
+};
+
+// exports.getReset = (req, res) => {
+//   res.render("auth/reset", {
+//     path: "/reset",
+//     pageTitle: "Reset Password",
+//     errorMessage: req.flash("error")?.[0],
+//   });
+// };
+
+exports.getReset = (req, res, next) => {
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "reset",
+    isAuthenticated: false,
+    errorMessage: req.flash("error")?.[0],
+  });
+};
+
+exports.postReset = async (req, res, next) => {
+  crypto.randomBytes(32, async (error, buffer) => {
+    try {
+      if (err) return res.redirect("/reset");
+      const token = buffer.toString("hex");
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        req.flash("error", "No account with that email");
+        return res.redirect("/reset");
+      }
+      user.resetToken = token;
+      user.resetTokenExpiration = Date.now() + 3600000;
+      await user.save();
+      //send email
+      res.redirect("/");
+    } catch (error) {}
   });
 };
